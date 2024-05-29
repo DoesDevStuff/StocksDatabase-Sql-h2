4a05a0ab79c5c48b4638f34fc6a67f1d33dde86d
CharlieTheIndieDev <74312830+DoesDevStuff@users.noreply.github.com>
2024-05-29 21:35:11 +0100

    This is the new and final update
    We have now manipulated this so that it reads all the new csv files from the  csv folder. Which we have separately ensured is a fresh download per day
    
    This code now reads and compares if the file values exist if not it programmatically updates this
    
    We also have other queries here

--- src/H2TestMain.java
+++ src/H2TestMain.java
@@ -8,9 +8,6 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.List;
-
-import org.apache.commons.csv.CSVRecord;
 import org.h2.tools.Server;
 
 public class H2TestMain {
@@ -50,42 +47,12 @@ public class H2TestMain {
 				System.out.println("Unable to get connection, exiting...");
 				return;
 			}
-
 			
-			ReadStockCsvFiles readStockCsvFiles = new ReadStockCsvFiles();
 			DbUtils dbUtils = new DbUtils();
 			
-			//Deleting values in table
-			int rowsDeleted = dbUtils.deleteRows();
-			System.out.println("Number of records deleted from " + Constants.HISTORICAL_VALUE_TABLE + ": " + rowsDeleted + "\n");
-			
-			String[] stockeNamesArray = {"Reliance.Bse", "M&Mfin.Bse", "HdfcBank.Bse", "BajFinance.Bse"};
-			int stockeNamesArray_length = stockeNamesArray.length;
-			
-			for (int i = 0; i < stockeNamesArray_length; i++) {
-				String stockSymbol = stockeNamesArray[i];
-				
-				List<CSVRecord> csvRecords = readStockCsvFiles.getCSVRecordsForStock(stockSymbol);
-				int numberOfRecords = csvRecords.size();
-				System.out.println("Number of records in " + stockSymbol + " CSV file: " + numberOfRecords + "\n");
-
-				//CSV received from AVC API is arranged in descending order, but we want to insert stock rows
-				//in ascending order. So the method getInsertIntoHistoricalTableQuery() reads the records from
-				//the end of csvRecords so we are passing the last index in csvRecords - means insert all rows  
-				int fromIndex = (numberOfRecords - 1);
-				
-				//get the complete insert query
-				String insertQuery = dbUtils.getInsertIntoHistoricalTableQuery(stockSymbol, csvRecords, fromIndex);
-				
-				if (insertQuery == null) {
-					System.out.println("Unable to get insert query, exiting...");
-					return;
-				}
-				
-				int rowsInserted = dbUtils.insertRows(insertQuery);
-				System.out.println("Number of records inserted for " + stockSymbol + ": " + rowsInserted  + "\n");
-			}
-			
+			String[] stockNamesArray = {"HdfcBank.Bse", "BajFinance.Bse"};
+			//String[] stockNamesArray = {"Reliance.Bse", "M&Mfin.Bse", "HdfcBank.Bse", "BajFinance.Bse"};
+			dbUtils.insertMultipleCsv(stockNamesArray);
 		}
 		catch (Exception ex) {
 			System.out.println("Exception: " + ex.getMessage());
